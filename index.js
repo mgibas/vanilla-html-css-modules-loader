214d5d24c4886d0d7d9f20308ec48c07317ecfb8
Maciej Gibas <gibasmaciej@gmail.com>
2018-08-27 11:03:52 +0200

    naive implementation

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const fs = require('fs')
+const classRegex = /<.*(class="[\w-\s]*").*>/gm
+
 module.exports = function(content, map, meta) {
-  console.log(meta)
-  return content
+  let mapPath = this.resourcePath.replace('.html','.scss.json')
+  let map = JSON.parse(fs.readFileSync(mapPath))
+  let mapClasses = Object.keys(map) 
+
+  let result = content
+  let match = classRegex.exec(content)
+  while (match != null) {
+    let mapped = match[1]
+    for(let c of mapClasses) {
+      mapped = mapped.replace(c, map[c])  
+    }
+    result = result.replace(match[1], mapped)
+    match = classRegex.exec(content);
+  }
+  return result
 };
